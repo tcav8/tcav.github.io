e670d65e9780350bda20baa0e7573068093116eb
3dizhi <48689100+3dizhi@users.noreply.github.com>
2019-03-27 23:17:29 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -13,13 +13,26 @@ var otherUrls = [
 var foreverUrls = [
     'https://julyporn.com',
     'https://www.julyporn.com',
-]
+];
+var notices = [
+    '* 我们推荐电脑以及Andriod手机用户使用Chrome(谷歌)浏览器浏览本站，iPhone手机我们建议使用手机自带的Safria浏览器访问本站。',
+    '* 为了防止域名劫持而无法访问，请设置手机和电脑的的DNS，我们推荐使用8.8.8.8和1.1.1.1。',
+    '* 大陆地区用户我们建议您可以使用VPN和代理的方式来访问我们的永久地址。',
+];
 
-function createFieldElem(title, items, plainText) {
+function createFieldElem(option) {
+    var title = option.title;
+    var items = option.items;
+    var plainText = option.plainText;
+    var classStr = option.classStr;
+    var text = option.text;
+
     var fieldElem = document.createElement('div');
-    fieldElem.setAttribute('class', 'field');
+    var fieldClass = ['field', classStr].join(' ');
+    fieldElem.setAttribute('class', fieldClass);
 
     var titleElem = document.createElement('h4');
+    titleElem.setAttribute('class', 'title');
     titleElem.innerHTML = title;
     fieldElem.appendChild(titleElem);
 
@@ -32,6 +45,12 @@ function createFieldElem(title, items, plainText) {
             htmlStr = htmlStr + '<li><a href="' + items[i] + '" target="_blank">' + items[i] + '</a></li>';
         }
     }
+
+    // For text
+    if (text) {
+        htmlStr = htmlStr + '<li class="text">' + text + '</li>';
+    }
+
     ulElem.innerHTML = htmlStr;
     fieldElem.appendChild(ulElem);
     return fieldElem;
@@ -41,23 +60,28 @@ window.onload = function () {
     var mainElem = document.getElementById('main');
     // append logo
     var logoElem = document.createElement('div');
+    logoElem.setAttribute('class', 'brand');
     logoElem.setAttribute('id', 'logo');
-    logoElem.innerHTML = 'JULY<span>PORN</span>'
+    logoElem.innerHTML = 'JULY <span>PORN</span><span class="flag">七色</span>'
     mainElem.appendChild(logoElem);
 
     // newest urls
-    var newestFieldElem = createFieldElem('最新訪問地址', newestUrls);
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问我们的网站'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
-    var otherFieldElem = createFieldElem('其他地址發佈', otherUrls);
+    var otherFieldElem = createFieldElem({title: '收藏地址发布页', items: otherUrls});
     mainElem.appendChild(otherFieldElem);
 
     // email
-    var mailFieldElem = createFieldElem('通過郵件獲取', emails, true);
+    var mailFieldElem = createFieldElem({title: '发送邮件获取', items: emails, plainText: true});
     mainElem.appendChild(mailFieldElem);
 
     // forever urls
-    var foreverFieldElem = createFieldElem('永久訪問地址', foreverUrls);
+    var foreverFieldElem = createFieldElem({title: '永久地址', items: foreverUrls});
     mainElem.appendChild(foreverFieldElem);
+
+    // Description
+    var noticeFieldElem = createFieldElem({title: '注意事项', items: notices, plainText: true, classStr: 'desc'});
+    mainElem.appendChild(noticeFieldElem);
 }
